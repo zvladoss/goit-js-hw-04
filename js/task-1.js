5a2048da2ef67a79d3714d98112417378de19d71
Vladislav Zarovniy <121088289+zvladoss@users.noreply.github.com>
2023-12-19 18:58:23 +0200

    Initial commit

---
+++ js/task-1.js
@@ -0,0 +1,18 @@
+'use strict';
+
+
+function slugify(title) {
+
+  const words = title.toLowerCase().split(' ');
+
+
+  const slug = words.join('-');
+
+  return slug
+}
+
+
+console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
